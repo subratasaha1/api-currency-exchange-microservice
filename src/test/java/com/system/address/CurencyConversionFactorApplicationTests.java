60dacb2c4f45e4515fcb015fb2f574ae3c629e79
subrata <ju2005.subrata@gmail.com>
2023-06-26 22:18:04 +0100

    First draft files

---
+++ src/test/java/com/system/address/CurencyConversionFactorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.address;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurencyConversionFactorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
